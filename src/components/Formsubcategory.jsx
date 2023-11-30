dd6602d7001e8225fd1cd3549d7cb1dd34fd251a
Diego Medina <diegoleonmedina62@gmail.com>
2023-11-30 11:57:55 -0300

    add form to add and save new subcategories

---
+++ src/components/Formsubcategory.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import axios from "axios"
+const url = "http://localhost:3000/subcategories"
+
+const Formsubcategory = ({ item, getSubcategories }) => {
+  const [newItemName, setNewItemName] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!newItemName) return
+    try {
+      const response = await axios.post(url, {
+        SubcategoryName: newItemName,
+        CategoryIdKey: item.id_key,
+      })
+      setNewItemName("")
+      getSubcategories()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="my-2">
+        <input
+          type="text"
+          value={newItemName}
+          className="rounded-full border border-black p-1"
+          onChange={(e) => {
+            setNewItemName(e.target.value)
+          }}
+        />
+        <button
+          type="submit"
+          className="mx-4 rounded-full bg-gray-300 px-4 hover:bg-purple-500"
+        >
+          agregar
+        </button>
+      </div>
+    </form>
+  )
+}
+export default Formsubcategory
