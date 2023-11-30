12e1e525405e1cc8ca3daaf8cd3f8e9504d57f49
Diego Medina <diegoleonmedina62@gmail.com>
2023-11-30 12:16:46 -0300

    individual categories display and subjects divs

--- src/components/Individualsubcat.jsx
+++ src/components/Individualsubcat.jsx
@@ -1,4 +1,19 @@
 const Individualsubcat = ({ item }) => {
-  return <div>{item.SubcategoryName}</div>
+  return (
+    <section className="ml-20 border border-gray-300 p-2">
+      <div className="  my-4 flex">
+        <h2>{item.SubcategoryName}</h2>
+        <button
+          className="mx-4 rounded-full bg-gray-300 px-4 hover:bg-green-300"
+          onClick={() => {}}
+        >
+          Borrar
+        </button>
+      </div>
+      <div className="relative ml-16 border border-gray-300 p-2">
+        <h3>Temas</h3>
+      </div>
+    </section>
+  )
 }
 export default Individualsubcat
